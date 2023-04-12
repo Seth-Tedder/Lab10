991d026b1de59f9cb1348083eb322d19046ffd64
Seth-Tedder <126044310+Seth-Tedder@users.noreply.github.com>
2023-04-12 10:15:02 -0400

    Add files via upload

---
+++ HashTable.java
@@ -0,0 +1,20 @@
+package lab10;
+
+/**
+ * HashTable class
+ * @author Seth, Kyrin, Abbas
+ * @version
+ */
+public class HashTable<E> {
+	private E[] table;
+	
+	public HashTable(E item) {
+		
+	}
+	public boolean add() {
+		return false;
+	}
+	public E find() {
+		return null;
+	}
+}
